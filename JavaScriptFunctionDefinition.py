50556ee4c6759563057713ffaa854c1a6dd47127
asdf23 <andrew.mainland@gmail.com>
2014-08-03 21:18:26 -0700

    Update JavaScriptFunctionDefinition.py
    Adds meta data when autocomplete completes. Meta-data must be in format of triple forward slashes before function name. First triple slashed line must contain the function name. Additional triple slashed prefixed lines are available. followed by the word function space(s) function name and function parameters.See jsExample.js

--- JavaScriptFunctionDefinition.py
+++ JavaScriptFunctionDefinition.py
@@ -1,13 +1,12 @@
 import sublime, sublime_plugin, functools
 
 class JavaScriptFunctionDefinition(sublime_plugin.EventListener):
-	#pending = 0
+	pumpPrimed = False
+	autoCompleteText = ""
 	def on_load(self, view):
 		if "JavaScript" not in view.settings().get("syntax"): return
 	def on_modified(self, view):
 		if "JavaScript" not in view.settings().get("syntax"): return
-		#self.pending = self.pending + 1
-		#sublime.set_timeout(functools.partial(self.handleTimeout, view), 10000)
 	def on_query_completions(self, view, prefix, locations):
 		if "JavaScript" not in view.settings().get("syntax"): return
 		matches = view.find_all("function {0}[a-zA-Z\\$0-9_]*\\([a-zA-Z\\$0-9_, \t]*\\)".format(prefix))
@@ -18,15 +17,30 @@ class JavaScriptFunctionDefinition(sublime_plugin.EventListener):
 		for match in matches:
 			functionParts = view.substr(match).split("(")
 			functionName = functionParts[0].split("function")[1].strip()
-			results.append(("__{0}\t({1}".format(functionParts[0], functionParts[1]), "{0}({1};".format(functionName,functionParts[1])))
+			results.append(("{0}\t({1}".format(functionParts[0], functionParts[1]), "{0}({1};".format(functionName,functionParts[1])))
+		self.pumpPrimed = (len(matches) > 0)
+		if len(matches) > 0:
+			self.autoCompleteText = functionName
 		return (results, sublime.INHIBIT_WORD_COMPLETIONS)
-	""" Function summary meta data is coming....
-	def handleTimeout(self, view):
-		self.pending = self.pending - 1
-		if self.pending == 0:
-			self.onIdle(view)
-	def onIdle(self, view):
-		print("No activity in the past 10000ms")
-	def refreshList(self, view):
-		print("refreshList not implemetned")
-	"""
+	def on_post_text_command(self, view, command_name, args):
+		if "JavaScript" not in view.settings().get("syntax"): return
+		if((command_name == "insert_best_completion") and self.pumpPrimed):
+			DisplayFunctionMetaDataCommand.popUpMenu(self, view, self.autoCompleteText)
+
+class DisplayFunctionMetaDataCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		if (view.sel()[0].empty()): return
+		region = view.sel()[0]
+		selectedText = view.substr(region)
+		self.popUpMenu(self, view, selectedText)
+	@staticmethod
+	def popUpMenu(self, view, selectedText):
+		reg = "///[\s]*{0}[\s]*(\n///[\s\S]*\n)function[\s]+{0}[\s]*\([a-zA-Z\\$0-9_, \s]*\)".format(selectedText)
+		matches = view.find_all(reg)
+		if len(matches) >= 1:
+			functionDefinition = view.substr(matches[0]).split("\n")
+			functionDefinition = [w.strip("///") for w in  functionDefinition]
+			view.window().show_quick_panel(functionDefinition, None, 1, 2)
+	def on_done(self, index):
+		return
